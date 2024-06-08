0e7a28a0a9757a93d18b1b73262064f65cfbd186
farhang-hesami <far.hesami94@gmail.com>
2024-06-09 00:08:55 +0300

    first commit

--- lib/screens/profile/user_comments.dart
+++ lib/screens/profile/user_comments.dart
@@ -8,6 +8,7 @@ import 'package:flutter_application_diamond_shop_final/theme.dart';
 import 'package:flutter_application_diamond_shop_final/utils/date_formatter.dart';
 import 'package:flutter_application_diamond_shop_final/utils/navigator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class UserCommentsScreen extends StatefulWidget {
   const UserCommentsScreen({super.key});
@@ -32,87 +33,108 @@ class _UserCommentsScreenState extends State<UserCommentsScreen> {
       body: BlocBuilder<ProfileBloc, ProfileState>(
         builder: (context, state) {
           if (state is UserCommentsSuccess) {
-            return ListView.separated(
-              padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 50.0),
-              itemCount: state.userComments.length,
-              itemBuilder: (context, index) {
-                var comment = state.userComments[index];
-                return GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () {
-                    goScreen(
-                      context: context,
-                      screen: ProductDetailScreen(
-                        Product(
-                          id: comment.productId,
+            return state.userComments.isEmpty
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/comments.svg',
+                          width: 250.0,
+                          height: 250.0,
                         ),
-                      ),
-                      fullScreen: true,
-                    );
-                  },
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CachedNetworkImage(
-                        imageUrl:
-                            'https://flutter.vesam24.com/${comment.productImage}',
-                        width: 70.0,
-                        height: 70.0,
-                        placeholder: (context, url) => const Center(
-                          child: CupertinoActivityIndicator(),
+                        Text(
+                          'شما نظری ثبت نکرده اید.',
+                          style: Theme.of(context).textTheme.bodySmall,
                         ),
-                        errorWidget: (context, url, error) => const Center(
-                          child: Icon(Icons.error),
-                        ),
-                      ),
-                      const SizedBox(width: 12.0),
-                      Expanded(
-                        child: Column(
+                      ],
+                    ),
+                  )
+                : ListView.separated(
+                    padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 50.0),
+                    itemCount: state.userComments.length,
+                    itemBuilder: (context, index) {
+                      var comment = state.userComments[index];
+                      return GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () {
+                          goScreen(
+                            context: context,
+                            screen: ProductDetailScreen(
+                              Product(
+                                id: comment.productId,
+                              ),
+                            ),
+                            fullScreen: true,
+                          );
+                        },
+                        child: Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              comment.productTitle!,
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
-                            Text(
-                              '${convertDateAndTime(comment.createDate!).$2} - ${convertDateAndTime(comment.createDate!).$1}',
-                              style: Theme.of(context).textTheme.labelSmall,
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width,
-                              padding: EdgeInsets.all(12.0),
-                              decoration: BoxDecoration(
-                                color: AppColors.kGray50,
-                                borderRadius: BorderRadius.circular(4.0),
+                            CachedNetworkImage(
+                              imageUrl:
+                                  'https://flutter.vesam24.com/${comment.productImage}',
+                              width: 70.0,
+                              height: 70.0,
+                              placeholder: (context, url) => const Center(
+                                child: CupertinoActivityIndicator(),
                               ),
+                              errorWidget: (context, url, error) =>
+                                  const Center(
+                                child: Icon(Icons.error),
+                              ),
+                            ),
+                            const SizedBox(width: 12.0),
+                            Expanded(
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
                                     comment.productTitle!,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodySmall!
-                                        .copyWith(
-                                          fontWeight: FontWeight.w700,
-                                        ),
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
                                   ),
-                                  Text(comment.text!)
+                                  Text(
+                                    '${convertDateAndTime(comment.createDate!).$2} - ${convertDateAndTime(comment.createDate!).$1}',
+                                    style:
+                                        Theme.of(context).textTheme.labelSmall,
+                                  ),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    padding: EdgeInsets.all(12.0),
+                                    decoration: BoxDecoration(
+                                      color: AppColors.kGray50,
+                                      borderRadius: BorderRadius.circular(4.0),
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          comment.subject!,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall!
+                                              .copyWith(
+                                                fontWeight: FontWeight.w700,
+                                              ),
+                                        ),
+                                        Text(comment.text!)
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 6.0),
                                 ],
                               ),
                             ),
-                            const SizedBox(height: 6.0),
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider();
-              },
-            );
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                  );
           } else if (state is UserCommentsError) {
             return Center(
               child: Text(state.error),
